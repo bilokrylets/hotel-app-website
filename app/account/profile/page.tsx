f24b1f55f7859d1a7d0ef45c6f4f7a2caa950e4d
Yaroslav Bilokrylets <4believy@gmail.com>
2024-09-09 16:21:30 +0300

    feat: add nested layout for account

--- app/account/profile/page.tsx
+++ app/account/profile/page.tsx
@@ -1,5 +1,9 @@
 import SelectCountry from "@/app/_components/SelectCountry";
 
+export const metadata = {
+  title: "Update profile",
+};
+
 type Props = {};
 function Profile({}: Props) {
   const countryFlag = "pt.jpg";
--- app/account/reservations/page.tsx
+++ app/account/reservations/page.tsx
@@ -1,5 +1,9 @@
 import ReservationCard from "@/app/_components/ReservationCard";
 
+export const metadata = {
+  title: "Reservations",
+};
+
 type Props = {};
 function Reservations({}: Props) {
   const bookings = [];
