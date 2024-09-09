19f5e5d70c814036579414633df5551d8948e9a8
Yaroslav Bilokrylets <4believy@gmail.com>
2024-09-09 16:05:49 +0300

    feat: add about page routes

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,9 +1,13 @@
 export const metadata = {
-  title: 'Account',
+  title: "Account",
 };
 
 type Props = {};
 function Account({}: Props) {
-  return <div>Account</div>;
+  return (
+    <h2 className="mb-7 text-2xl font-semibold text-accent-400">
+      Welcome $Username
+    </h2>
+  );
 }
 export default Account;
