a4be2a6598bb555189c5d5605c6f511b0831ea61
Yaroslav Bilokrylets <4believy@gmail.com>
2024-09-17 12:41:33 +0300

    feat: add signIn and signOut button functionality

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,12 +1,15 @@
+import { auth } from "../_lib/auth";
+
 export const metadata = {
   title: "Account",
 };
 
 type Props = {};
-function Account({}: Props) {
+async function Account({}: Props) {
+  const session = await auth();
   return (
     <h2 className="mb-7 text-2xl font-semibold text-accent-400">
-      Welcome $Username
+      Welcome, {session?.user?.name}
     </h2>
   );
 }
